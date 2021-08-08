e4e19370ca20317dc8473eb28aad614ba0d337a5
Artem Klyukvin <aklyukvin@yandex.ru>
2021-08-08 15:19:34 +0300

    use spaces in formatting (#6)

--- pkg/snippet/snippetformat/snippet_format.go
+++ pkg/snippet/snippetformat/snippet_format.go
@@ -1,6 +1,9 @@
 package snippetformat
 
-import "go/format"
+import (
+	"go/format"
+	"strings"
+)
 
 func FormatSnippet(text string) (string, error) {
 	b, err := format.Source([]byte(text))
@@ -8,5 +11,7 @@ func FormatSnippet(text string) (string, error) {
 		return "", err
 	}
 
-	return string(b), nil
+	// dirty hack to use 4 spaces in formatting instead of tabs
+	// i hope no one uses plain tabs inside their strings in lessons
+	return strings.ReplaceAll(string(b), "\t", "    "), nil
 }
